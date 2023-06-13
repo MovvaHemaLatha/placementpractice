a5c26426578d599a61b0b8c859e8870c20fa05fd
MovvaHemaLatha <125755411+MovvaHemaLatha@users.noreply.github.com>
2023-06-13 09:57:33 +0530

    D:\placements\largestnumberamong3numbers.java

---
+++ evenodd.java
@@ -0,0 +1,18 @@
+//even or odd
+import java.util.*;
+class evenodd
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+System.out.println("enter n");
+if(n%2==0)
+{
+System.out.println("even");
+}
+else{
+System.out.println("odd");
+}}}
+
+ 
