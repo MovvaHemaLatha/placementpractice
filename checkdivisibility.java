2686ac80db5cafd98f80eadfb71c66601e96a3b1
MovvaHemaLatha <125755411+MovvaHemaLatha@users.noreply.github.com>
2023-06-13 11:31:33 +0530

    Add files via upload

---
+++ checkdivisibility.java
@@ -0,0 +1,21 @@
+//no of integers divisible by 5
+import java.util.*;
+class checkdivisibility
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter num:");
+int num=sc.nextInt();
+if(num%5==0)
+{
+System.out.println("divisible by 5");
+}
+else
+{
+System.out.println("not divisible by 5");
+}
+}
+}
+
+
