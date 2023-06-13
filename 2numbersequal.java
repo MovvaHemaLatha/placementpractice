62335414ac665e50f978edbc602513004fb7b501
MovvaHemaLatha <125755411+MovvaHemaLatha@users.noreply.github.com>
2023-06-13 11:40:14 +0530

    Add files via upload

---
+++ 2numbersequal.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class 2numbersequal
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter n1");
+int n1=sc.nextInt();
+System.out.println("enter n2");
+int n2=sc.nextInt();
+if(n1==n2)
+{
+System.out.println("equal numbers");
+}
+else
+{
+System.out.println("not equal");
+}
+}
+}
